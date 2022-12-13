018725155f971d858191bc4cacfb107f4de17a92
Renato Teixeira <renato.teixeira.93@gmail.com>
2022-12-13 13:42:06 +0000

    Fix: missing associations, getters

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
@@ -40,6 +40,9 @@ public class Offer implements Serializable {
 	@OneToMany(mappedBy = "offer")
 	private List<Resource> resources = new ArrayList<>();
 	
+	@OneToMany(mappedBy = "offer")
+	private List<Topic> topics = new ArrayList<>();
+	
 	public Offer() {
 		
 	}
@@ -96,11 +99,15 @@ public class Offer implements Serializable {
 		return resources;
 	}
 
+	public List<Topic> getTopics() {
+		return topics;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
